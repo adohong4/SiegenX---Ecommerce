0881738a9f97b2b2d7c83239a09927d7d52325e0
adohong4 <adohong4@gmail.com>
2024-11-24 22:31:37 +0700

    first commit

---
+++ backend/src/app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const connectDB = require('./config/db.mongo');
+const { default: helmet } = require('helmet')
+const compression = require('compression')
+const morgan = require('morgan')
+const app = express()
+
+//init middlewares
+app.use(morgan("dev"))
+app.use(helmet())
+app.use(compression())
+app.use(express.json())
+
+//init db
+connectDB();
+
+//handling error
+app.use((req, res, next) => {
+    const error = new Error('Not Found')
+    error.status = 404
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    const statusCode = error.status || 500
+    return res.status(statusCode).json({
+        status: 'error',
+        code: statusCode,
+        stack: error.stack,
+        message: error.message || 'Internal Server Error'
+    })
+})
+
+module.exports = app
