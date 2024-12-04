827c69a9b0865a7ea1e3df9ab4097d8578deb820
adohong4 <adohong4@gmail.com>
2024-12-05 00:12:19 +0700

    contact

--- frontend-user/src/App.jsx
+++ frontend-user/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home/Home'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
 import Introduce from './pages/Introduce/introduce'
+import Contact from './pages/Contact/Contact'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<Introduce />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
       </div>
--- frontend-user/src/assets/assets.js
+++ frontend-user/src/assets/assets.js
@@ -14,8 +14,12 @@ import about3 from './about/about3.png'
 import about4 from './about/about4.png'
 import economic from './about/economic.png'
 
+import contact from './banner/contact.png'
+import introduce from './banner/introduce.png'
+
 export const assets = {
     logo,
     icon1, icon2, icon3, icon4, icon5, icon6, arrowRight,
-    about1, about2, about3, about4, economic
+    about1, about2, about3, about4, economic,
+    contact, introduce
 }
